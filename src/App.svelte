99c6735d2ed4b8eeca8a754803528117de091ba8
nico-mayer <nico.m94@icloud.com>
2022-07-30 21:27:38 +0200

    implemented functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,29 @@
 <script>
   import Editor from "./lib/components/Editor.svelte"
+
+  let html = "<h1>Hello World</h1>"
+  let css = "body { color: red; }"
+  let js = "document.body.style.background = 'blue'"
+  let srcDoc = ``
+  let cooldownTimer
+  $: {
+    clearTimeout(cooldownTimer)
+    cooldownTimer = setTimeout(async () => {
+      srcDoc = `<html><body>${html}</body><style>${css}</style><script>${js}<\/script></html>`
+    }, 320)
+  }
 </script>
 
-<main>
-  <section class="pain top-pain">
-    <Editor mode={"htmlmixed"} />
-    <Editor mode={"css"} />
-    <Editor mode={"javascript"} />
+// @ts-nocheck
+<main class="w-screen h-screen">
+  <section class="pane top-pane flex w-full space-x-4">
+    <Editor title={"HTML"} lang={"xml"} bind:value={html} />
+    <Editor title={"CSS"} lang={"css"} bind:value={css} />
+    <Editor title={"JS"} lang={"javascript"} bind:value={js} />
   </section>
-  <section class="pain">
+  <section class="pane">
     <iframe
+      {srcDoc}
       title="output"
       sandbox="allow-scripts"
       frameborder="0"
@@ -18,3 +32,10 @@
     />
   </section>
 </main>
+
+<style>
+  .pane {
+    height: 50%;
+    background-color: white;
+  }
+</style>
--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -1,60 +1,59 @@
 <script>
+  // @ts-nocheck
   import "codemirror/lib/codemirror.css"
+  import "codemirror/theme/material.css"
   import "codemirror/mode/javascript/javascript"
   import "codemirror/mode/css/css"
-  import "codemirror/mode/htmlmixed/htmlmixed"
+  import "codemirror/mode/xml/xml"
   import CodeMirror from "codemirror"
-  import { onMount } from "svelte"
+  import { onMount, beforeUpdate } from "svelte"
 
-  export let value = ""
-
-  export let mode
+  export let value
+  export let lang
+  export let title
   let textArea
 
   onMount(() => {
     const editor = CodeMirror.fromTextArea(textArea, {
       lineWrapping: true,
-      mode: mode,
+      mode: lang,
       smartIndent: true,
       lineNumbers: true,
+      theme: "material",
     })
-    // @ts-ignore
     editor.focus()
+    editor.on("change", () => {
+      value = editor.getValue()
+    })
     return () => {
-      // @ts-ignore
       editor.toTextArea()
     }
   })
 </script>
 
 <div class="editor-container">
-  <textarea bind:this={textArea} {value} readonly />
+  <div class="top-bar">
+    <span>{title}</span>
+    <button>O/C</button>
+  </div>
+
+  <textarea bind:this={textArea} bind:value readonly />
 </div>
 
 <style>
   .editor-container {
-    margin: 2rem auto 0;
-    width: 800px;
-    padding: 2rem;
-    background: white;
-    border-radius: 0.5rem;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    background-color: #22272e;
+    color: white;
+    flex-grow: 1;
+    flex-basis: 0;
+    border-radius: 1rem;
+    overflow: hidden;
   }
-
-  .editor-container :global(.CodeMirror-cursor) {
-    border-left: 4px solid rgb(26, 154, 240);
-  }
-
-  .editor-container :global(.cm-header) {
-    color: black;
-  }
-  .editor-container :global(.cm-header-1) {
-    font-size: 200%;
-  }
-  .editor-container :global(.cm-header-2) {
-    font-size: 150%;
-  }
-  .editor-container :global(.cm-header-3) {
-    font-size: 120%;
+  .top-bar {
+    padding: 10px 30px;
+    display: flex;
+    justify-content: space-between;
   }
 </style>
