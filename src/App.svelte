70fe75e7f6955a884685943b3f5eaf7afdfc176b
nico-mayer <nico.m94@icloud.com>
2022-07-31 09:42:55 +0200

    impemented resize sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script>
   import Editor from "./lib/components/Editor.svelte"
 
-  let html = "<h1>Hello World</h1>"
+  let html = "<h1 class='text-white font-mono text-7xl'>Hello World</h1>"
   let css = ""
   let js = "document.body.style.background = '#263238'"
   let cooldownTimer
+  let sidebar
   let iframe
+
+  let resizer
+  let resizing = false
 
   $: {
     clearTimeout(cooldownTimer)
@@ -26,15 +30,45 @@
       output.close()
     }, 320)
   }
+
+  function handleMouseDown() {
+    resizing = true
+    window.addEventListener("mousemove", handleMouseMove)
+    window.addEventListener("mouseup", handleMouseUp)
+
+    function handleMouseMove(e) {
+      if (e.clientX > 300) {
+        sidebar.style.width = e.clientX + "px"
+        document.body.style.cursor = "col-resize"
+      }
+    }
+    function handleMouseUp() {
+      resizing = false
+      document.body.style.cursor = "auto"
+      window.removeEventListener("mousemove", handleMouseMove)
+      window.removeEventListener("mouseup", handleMouseUp)
+    }
+  }
 </script>
 
 <main class="w-screen h-screen flex">
-  <section class="flex flex-col w-[410px] space-y-4 bg-[#22272E]">
-    <Editor title={"HTML"} lang={"xml"} bind:value={html} />
-    <Editor title={"CSS"} lang={"css"} bind:value={css} />
-    <Editor title={"JS"} lang={"javascript"} bind:value={js} />
+  <section bind:this={sidebar} class="sidebar flex w-[472px] bg-[#22272E]">
+    <div class="flex flex-col w-full space-y-4 ">
+      <Editor title={"HTML"} lang={"xml"} bind:value={html} />
+      <Editor title={"CSS"} lang={"css"} bind:value={css} />
+      <Editor title={"JS"} lang={"javascript"} bind:value={js} />
+    </div>
+    <div
+      bind:this={resizer}
+      on:mousedown={handleMouseDown}
+      class="flex h-full w-[20px] cursor-col-resize border-white/20 border-x"
+    />
   </section>
-  <section class="flex flex-1">
+
+  <section class="flex flex-1 relativ">
+    {#if resizing}
+      <div class="bg-transparent w-full h-full absolute left-0 top-0" />
+    {/if}
     <iframe
       bind:this={iframe}
       title="output"
