31fabf329b2529c67992fd891f182644de04fcd6
nico-mayer <nico.m94@icloud.com>
2022-08-03 13:39:06 +0200

    init darkmode toggle styling

--- src/lib/components/DarkModeToggle.svelte
+++ src/lib/components/DarkModeToggle.svelte
@@ -1,12 +1,10 @@
 <script>
-	let darkMode = false;
+	let darkMode = true;
 </script>
 
-<main
-	class="w-18 h-8 bg-[#1F2430] flex items-center rounded-full p-2 dark:justify-end justify-start text-base"
->
+<main class="w-16 h-8 dark:bg-black bg-white flex items-center rounded-full text-base relative">
 	<button
-		class="bg-[#cccac2] rounded-full h-6 w-6 flex justify-center items-center"
+		class="bg-black dark:bg-[#cccac2] rounded-full h-6 w-6 flex justify-center items-center transition-all duration-700 absolute left-[4px] dark:left-[36px]"
 		on:click={() => document.documentElement.classList.toggle('dark')}
 	>
 		<div class="i-carbon-sun dark:i-carbon-moon text-black" />
