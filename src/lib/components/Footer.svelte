efbac2144d52ae28ad26381fbd24fc23bca8d882
Nico-Mayer <nico.m94@icloud.com>
2022-08-03 20:48:09 +0200

    small style changes

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,1 +1,1 @@
-<div class="dark:(bg-[#1c212b] border-white/5) border-t border-black/20 h-7 w-full" />
+<div class="dark:(bg-[#1c212b] border-white/5) bg-[#F7F7F7] border-t border-black/20 h-7 w-full" />
