543b00cc9c830825687b535fccef4d659b0205da
Nico Mayer <nico.mayer@arsmedium.com>
2024-09-24 11:59:29 +0200

    chore(project): updated deps

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import Unocss from 'unocss/vite';
-import { extractorSvelte } from '@unocss/core';
-import transformerDirective from '@unocss/transformer-directives';
+import extractorSvelte from '@unocss/extractor-svelte';
 import presetIcons from '@unocss/preset-icons';
 import presetUno from '@unocss/preset-uno';
 import presetWebFonts from '@unocss/preset-web-fonts';
+import transformerDirective from '@unocss/transformer-directives';
 import transformerVariantGroup from '@unocss/transformer-variant-group';
+import Unocss from 'unocss/vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
